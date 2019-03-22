7c11f6853ce67a7134d95f45f5ac261b3ad8da5a
Robin Hillyard <scalaprof@gmail.com>
2019-03-21 23:57:38 -0400

    More refactoring.
    Split LineParser from RowParser.
    Improved Scaladoc.

---
+++ src/main/scala/com/phasmidsoftware/tableparser/LineParser.scala
@@ -0,0 +1,34 @@
+package com.phasmidsoftware.tableparser
+
+import scala.util.Try
+import scala.util.matching.Regex
+import scala.util.parsing.combinator.JavaTokenParsers
+
+/**
+  * LineParser: class to parse lines of a CSV file.
+  *
+  * @param delimiter a Regex used to match a delimiter between cells in a row.
+  * @param string    a Regex used to match the content of a cell.
+  * @param quote     the quote character which is able to preempt the string regex:
+  *                  between two quote characters,
+  *                  there can be any number of any character (other than quote).
+  */
+class LineParser(delimiter: Regex, string: Regex, quote: Char) extends JavaTokenParsers {
+  override def skipWhitespace: Boolean = false
+
+  def parseRow(w: String): Try[Seq[String]] = parseAll(row, w) match {
+    case Success(s, _) => scala.util.Success(s)
+    case Failure(x, _) => scala.util.Failure(ParserException(x))
+    case Error(x, _) => scala.util.Failure(ParserException(x))
+  }
+
+  def row: Parser[Seq[String]] = rep1sep(cell, delimiter)
+
+  def cell: Parser[String] = quotedString | string | failure("invalid string")
+
+  def quotedString: Parser[String] = quote ~> s"""[^$quote]*""".r <~ quote
+
+  override def toString: String = s"""LineParser: delimiter=$delimiter, string=$string, quote="$quote""""
+}
+
+case class ParserException(msg: String) extends Exception(msg)
---
+++ src/test/scala/com/phasmidsoftware/tableparser/LineParserSpec.scala
@@ -0,0 +1,25 @@
+package com.phasmidsoftware.tableparser
+
+import org.scalatest.FlatSpec
+
+class LineParserSpec extends FlatSpec {
+
+  behavior of "LineParserSpec"
+
+  it should "cell" in {
+
+  }
+
+  it should "quotedString" in {
+
+  }
+
+  it should "row" in {
+
+  }
+
+  it should "parseRow" in {
+
+  }
+
+}
