d84e5f26fd9d03df7abd268016fbdc7cc7184c42
Robin Hillyard <r.hillyard@northeastern.edu>
2025-06-10 16:25:01 -0400

    Running DatasetMapper as a main program is successful.
    However, running the equivalent unit test in the DatasetMapperSpec is not successful.

--- spark/src/main/scala/com/phasmidsoftware/tableparser/spark/DatasetMapper.scala
+++ spark/src/main/scala/com/phasmidsoftware/tableparser/spark/DatasetMapper.scala
@@ -1,38 +1,43 @@
 package com.phasmidsoftware.tableparser.spark
 
-import com.phasmidsoftware.tableparser.core.examples.{Format, Movie, Production}
-import com.phasmidsoftware.tableparser.core.parse.StandardRowParser
+import com.phasmidsoftware.tableparser.core.examples.Movie
 import com.phasmidsoftware.tableparser.core.table.Header
 import org.apache.spark.api.java.function.MapFunction
-import org.apache.spark.sql.catalyst.encoders.encoderFor
 import org.apache.spark.sql.{Dataset, Encoder, Encoders, SparkSession}
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
+
+class DatasetMapper[T](f: String => Try[T])(missing: T)(implicit sparkSession: SparkSession, encoder: Encoder[T]) extends Serializable {
+
+  def as(path: String): Dataset[T] = {
+
+    val mapFunction = new MapFunction[String, T] {
+      def call(value: String): T =
+        f(value) match {
+          case Success(t) => t
+          case Failure(exception) =>
+            System.err.println(exception.getLocalizedMessage)
+            missing
+        }
+    }
+
+    val sd = sparkSession.read.textFile(path)
+    sd.map(mapFunction, encoder)
+  }
+
+  def doMain(filename: String): Unit = {
+    val sd: Dataset[T] = as(filename)
+    sd.show(20)
+  }
+}
 
 object DatasetMapper extends App {
-
-  val spark: SparkSession = SparkSession.builder.appName("DatasetMapper").master("local[*]").getOrCreate()
-
-  val sd: Dataset[String] = spark.read.textFile("/Users/rhillyard/IdeaProjects/TableParser/spark/src/main/resources/com/phasmidsoftware/tableparser/spark/movie_metadata.csv")
-
+  implicit val spark: SparkSession = SparkSession.builder.appName("DatasetMapper").master("local[*]").getOrCreate()
+  implicit val encoder: Encoder[Movie] = Encoders.product[Movie]
   import com.phasmidsoftware.tableparser.core.examples.Movie._
 
   val parser = MovieTableParser.rowParser
+  val header = Header.create((Movie.header.split(',')): _*)
 
-  val header = Header.create((Movie.header.split(',')):_*)
+  new DatasetMapper[Movie](parser.parse(header))(Movie.missing).doMain("/Users/rhillyard/IdeaProjects/TableParser/spark/src/main/resources/com/phasmidsoftware/tableparser/spark/movie_metadata.csv")
 
-  private val mapFunction = new MapFunction[String, Movie] {
-    def call(value: String): Movie =
-      parser.parse(header)(value) match {
-        case Success(movie) => movie
-        case Failure(exception) =>
-          System.err.println(exception.getLocalizedMessage)
-          Movie.missing
-      }
-  }
-
-  private val encoder: Encoder[Movie] = Encoders.product[Movie]
-
-  val result = sd.map(mapFunction, encoder)
-
-  result.show(20)
 }
--- spark/src/test/scala/com/phasmidsoftware/tableparser/spark/DatasetMapperSpec.scala
+++ spark/src/test/scala/com/phasmidsoftware/tableparser/spark/DatasetMapperSpec.scala
@@ -1,2 +1,28 @@
 package com.phasmidsoftware.tableparser.spark
-class DatasetMapperSpec
+
+import com.phasmidsoftware.tableparser.core.examples.Movie
+import com.phasmidsoftware.tableparser.core.table.Header
+import org.apache.spark.sql.{Dataset, Encoder, Encoders, SparkSession}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class DatasetMapperSpec extends AnyFlatSpec with should.Matchers with Serializable {
+
+  behavior of "DatasetMapper"
+
+  it should "test" in {
+    implicit val spark: SparkSession = SparkSession.builder.appName("DatasetMapper").master("local[*]").getOrCreate()
+    implicit val encoder: Encoder[Movie] = Encoders.product[Movie]
+    import com.phasmidsoftware.tableparser.core.examples.Movie._
+
+    val parser = MovieTableParser.rowParser
+    val header = Header.create((Movie.header.split(',')): _*)
+
+    val target = new DatasetMapper[Movie](parser.parse(header))(Movie.missing)
+    // NOTE: I don't know if there's a way to specify a classpath resource in Spark so, for now, we define a totally non-portable filename
+    val filename = "/Users/rhillyard/IdeaProjects/TableParser/spark/src/main/resources/com/phasmidsoftware/tableparser/spark/movie_metadata.csv"
+    val dataset: Dataset[Movie] = target.as(filename)
+    dataset.show(20)
+    dataset.count() should be(20000)
+  }
+}
